e4cbf95e7099f6827d00c2c7c311216427569dc5
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-23 23:32:16 -0400

    X on square click

--- vite-tic-tac-toe/src/App.jsx
+++ vite-tic-tac-toe/src/App.jsx
@@ -3,25 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Board() {
-  const [count, setCount] = useState(0)
+function Square() {
+  const [squareClicked, setSquareClicked] = useState(null)
 
+  function handleSquareClick() {
+    setSquareClicked('X');
+  }
+
+  return <button onClick={handleSquareClick}>{squareClicked}</button>
+}
+
+function Board() {
+  //
   return (
     <>
       <div>
-        <button></button>
-        <button></button>
-        <button></button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
       <div>
-        <button></button>
-        <button></button>
-        <button></button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
       <div>
-        <button></button>
-        <button></button>
-        <button></button>
+        <Square ></Square>
+        <Square ></Square>
+        <Square ></Square>
       </div>
     </>
   )
