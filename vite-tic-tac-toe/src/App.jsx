e9cf9377e3ce8e55ce220216c5e4c61af88791f1
Erik <180784081+erhetrada@users.noreply.github.com>
2024-09-24 00:40:53 -0400

    refactored squre clicking behavior by implementing squareValues array, handleSquareClick in Board, pass into Square as props

--- vite-tic-tac-toe/src/App.jsx
+++ vite-tic-tac-toe/src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 
-function Square() {
-  const [squareClicked, setSquareClicked] = useState(null)
-
-  function handleSquareClick() {
-    setSquareClicked('X');
-  }
-
-  return <button onClick={handleSquareClick}>{squareClicked}</button>
+function Square({squareValue, handleSquareClick}) {
+  return <button onClick={handleSquareClick}>{squareValue}</button>
 }
 
 function Board() {
-  //
+  const [squareValues, setSquareValues] = useState(Array(9).fill(null));
+
+  function handleSquareClick(i) {
+    const updatedSquareValues = squareValues.slice();
+    updatedSquareValues[i] = 'X';
+    setSquareValues(updatedSquareValues);
+  }
+
   return (
     <>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square squareValue = {squareValues[0]} handleSquareClick = {() => handleSquareClick(0)} />
+        <Square squareValue = {squareValues[1]} handleSquareClick = {() => handleSquareClick(1)} />
+        <Square squareValue = {squareValues[2]} handleSquareClick = {() => handleSquareClick(2)} />
       </div>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square squareValue = {squareValues[3]} handleSquareClick = {() => handleSquareClick(3)} />
+        <Square squareValue = {squareValues[4]} handleSquareClick = {() => handleSquareClick(4)} />
+        <Square squareValue = {squareValues[5]} handleSquareClick = {() => handleSquareClick(5)} />
       </div>
       <div>
-        <Square ></Square>
-        <Square ></Square>
-        <Square ></Square>
+        <Square squareValue = {squareValues[6]} handleSquareClick = {() => handleSquareClick(6)} />
+        <Square squareValue = {squareValues[7]} handleSquareClick = {() => handleSquareClick(7)} />
+        <Square squareValue = {squareValues[8]} handleSquareClick = {() => handleSquareClick(8)} />
       </div>
     </>
   )
